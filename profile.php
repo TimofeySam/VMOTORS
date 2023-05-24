eb15f7e4f6da001615216249727d238144a8d098
TimofeySam <timofsamoshkin@gmail.com>
2023-05-24 19:52:34 +0300

    .

--- profile.php
+++ profile.php
@@ -1,16 +1,26 @@
-<?php
-    session_start()
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <title>Title</title>
-  </head>
-  <body>
-      <p>Имя: <? echo $_SESSION['name'];?></p>
-      <p>Фамилия: <? echo $_SESSION['lastname'];?></p>
-      <p>Email: <? echo $_SESSION['email'];?></p>
-      <p>id: <? echo $_SESSION['id'];?></p>
-  </body>
-</html>
+<!-- Конец меню -->
+<div class="container">
+    <p>Имя: <span id="name"></span></p>
+    <p>Фамиля: <span id="lastname"></span></p>
+    <p>Email: <span id="email"></span></p>
+    <p>ID: <span id="id"></span></p>
+    <form action="/uploadAvatar" enctype="multipart/form-data" method="post">
+        <input type="file" name="userfile">
+        <input type="submit">
+    </form>
+</div>
+<script>
+    let name = document.getElementById('name');
+    let lastname = document.getElementById('lastname');
+    let email = document.getElementById('email');
+    let id = document.getElementById('id');
+    fetch('/getUserData')
+        .then(function (response){return response.json()})
+        .then(function (result){
+            console.log(result);
+            name.innerText = result.name;
+            lastname.innerText = result.lastname;
+            email.innerText = result.email;
+            id.innerText = result.id;
+        });
+</script>
